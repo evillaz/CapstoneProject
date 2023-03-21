c15d9a4b095949293b1282912b6d6c3940a68a10
adarachel <adarachelofficial@gmail.com>
2023-03-21 12:45:34 +0100

    set up repo tools, webpack, jest and add header and footer

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+        testEnvironment: 'node', // Use the Node.js environment
+        testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], // Look for tests with these file name patterns
+        verbose: true, // Show detailed output    
+  };
