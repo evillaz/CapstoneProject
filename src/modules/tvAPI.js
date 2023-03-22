3db4bbb7766dc52be6eec3d8256741f4c69ee925
evillaz <evillaz123@gmail.com>
2023-03-21 20:47:32 -0500

    update display tvshows

---
+++ src/modules/tvAPI.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+class TvAPI {
+  baselink;
+  showList;
+
+  constructor() {
+    this.baselink = 'https://api.tvmaze.com/shows';
+    this.showList = [];
+  }
+
+  fetchShows = async () => {
+    try {
+      const response = await axios.get(this.baselink);
+      return response.data;
+    } catch(error) {
+      return error;
+    }
+  }
+
+  saveShowList = (shows) => {
+    this.showList = shows;
+  }
+  getShowList = () => this.showList;
+}
+
+const tvapi = new TvAPI;
+
+export default tvapi;
